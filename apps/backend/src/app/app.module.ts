860d09245b95f755e45403b9cd03813ccb3ecc4c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-09-20 17:12:23 +0300

    chore: update npm deps.

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BodyParserModule } from '@ditsmod/body-parser';
 import { HelloWorldModule } from './modules/routed/hello-world/hello-world.module.js';
 
 @rootModule({
-  providersPerApp: new Providers().useLogConfig({ level: 'debug' }),
+  providersPerApp: new Providers().useLogConfig({ level: 'info' }),
   appends: [HelloWorldModule],
   imports: [RoutingModule, BodyParserModule],
   exports: [RoutingModule, BodyParserModule],
